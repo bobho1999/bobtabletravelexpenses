19df09f96bbee80fbb1d5856c9e35639d92d36f5
Pocketart <shauncheon@shauncheon.com>
2017-05-08 09:21:33 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
@@ -20,6 +21,25 @@ using namespace std;
 // main program
 int main()
 {
+    // INITIALIZE VARIABLES
+
+    // Key variables
+    double total;
+    double mealTotal;
+    double spentTotal;
+
+    // employee info
+    string name;
+
+    // company spending limits
+    double allowedTotal;
+    double allowedParkingFee;
+    double allowedTaxiFee;
+    double allowedBreakfastFee;
+    double allowedLunchFee;
+    double allowedDinnerFee;
+    double allowedMealTotal;
+
     // variables for airplane costs
     double departureTime;
     double arrivalTime;
@@ -30,8 +50,14 @@ int main()
     double privatecarFee;
     double taxiFee;
 
+    // ask for name
+    cout << "Employee travel expense program by Bob, Jason, Jim, Oscar, and Shaun" << endl;
+    cout << "--------------------------------------------------------------------" << endl;
+    cout << "Enter the employee's name:";
+    cin >> name;
 
 
-    cout << "Hello, World!" << std::endl;
+
     return 0;
 }
+
