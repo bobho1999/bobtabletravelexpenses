b4a593d699b2d9650461fc21c69732c97c8aa6ba
Pocketart <shauncheon@shauncheon.com>
2017-05-08 09:38:27 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -30,8 +30,12 @@ int main()
 
     // employee info
     string name;
+    int days;
+    double departureTime;
+    double arrivalTime;
+    double airplaneFee;
 
-    // company spending limits
+    // company allowed spending limits
     double allowedTotal;
     double allowedParkingFee;
     double allowedTaxiFee;
@@ -41,22 +45,38 @@ int main()
     double allowedMealTotal;
 
     // variables for airplane costs
-    double departureTime;
-    double arrivalTime;
-    double airplaneFee;
 
     // variables for rental cars and private vehicle
     double rentalcarFee;
     double privatecarFee;
     double taxiFee;
 
-    // ask for name
+    // ask for name (maybe create a function for this)
     cout << "Employee travel expense program by Bob, Jason, Jim, Oscar, and Shaun" << endl;
     cout << "--------------------------------------------------------------------" << endl;
     cout << "Enter the employee's name:";
     cin >> name;
 
+    // ask for the length of the employee's trip (maybe create a function for this)
+    cout << "How many days long was " << name << "'s trip?" << endl;
+    cin >> days;
+    while (days < 1)
+    {
+        cout << "Invalid Input! The trip can't be less than 1 day long" << endl;
+        cout << "Enter a value larger than 1:";
+        cin >> days;
+    }
 
+    // ask for when the employee left for the trip (maybe create a function for this)
+    cout << "Please use the 0000 - 2400 format for time entries" << endl;
+    cout << "When did " << name << " leave for the trip?";
+    cin >> departureTime;
+    while (departureTime < 0 || departureTime > 2400)
+    {
+        cout << "Invalid Input! Time must be within the range of 0000 - 2400" << endl;
+        cout << "Enter a valid value:";
+        cin >> departureTime;
+    }
 
     return 0;
 }
