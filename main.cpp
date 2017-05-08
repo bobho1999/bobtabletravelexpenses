e8feadd56d9785765b105cbe3802d05e1ab9202a
Pocketart <shauncheon@shauncheon.com>
2017-05-08 08:44:39 +0800

    setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+/*
+ * C++ Travel Expenses group project
+ * Authors: 何柏翰, Jason Lin, Jim Yeh, Oscar Cheng, and Shaun Cheon
+ */
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+// function prototypes
+
+
+int main()
+{
+    cout << "Hello, World!" << std::endl;
     return 0;
 }
